f4a434d4eb5c9a56e5ed26068d6cea9595b2b52f
Wenderson <wps713@gmail.com>
2019-12-27 18:36:46 -0300

    Setting up routes in app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-// import { Container } from './styles';
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
-  );
+  return <Routes />;
 }
