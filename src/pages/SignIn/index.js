a58784649d25c30465db110aaa86525e2d1ddb4b
Wenderson <wps713@gmail.com>
2019-12-27 18:54:31 -0300

    Add background component in signin & up pages

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import Background from '~/components/Background';
+
 export default function SignIn() {
-  return <Text>Sign In</Text>;
+  return (
+    <Background>
+      <Text>Sign In</Text>
+    </Background>
+  );
 }
