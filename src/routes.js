493cf2e6fd0ce3911ac14623a091b18f464795a9
Wenderson <wps713@gmail.com>
2020-01-09 15:46:43 -0300

    Setting up  tabBar options

--- src/routes.js
+++ src/routes.js
@@ -14,9 +14,22 @@ export default (signedIn = false) =>
           SignIn,
           SignUp,
         }),
-        App: createBottomTabNavigator({
-          Dashboard,
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
         }),
+      },
+      {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#fff',
+              inactiveTintColor: 'rgba(255,255,255,0.6)',
+              style: {
+                backgroundColor: '#8d41a8',
+              },
+            },
+          }
+        ),
       },
       {
         initialRouteName: signedIn ? 'App' : 'Sign',
