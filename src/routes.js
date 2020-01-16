8aff98eaf7e2574d9218b3ff4c8c5e15ac2370f5
Wenderson <wps713@gmail.com>
2020-01-16 12:40:00 -0300

    Setting up New appointment pages in routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,17 @@
+import React from 'react';
+
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
+
+import SelectProvider from './pages/NewAppointment/SelectProvider';
+import SelectDateTime from './pages/NewAppointment/SelectDateTime';
+import Confirm from './pages/NewAppointment/Confirm';
 
 import Dashboard from '~/pages/Dashboard';
 import Profile from '~/pages/Profile';
@@ -18,6 +27,35 @@ export default (signedIn = false) =>
         App: createBottomTabNavigator(
           {
             Dashboard,
+            New: {
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirm,
+                },
+                {
+                  defaultNavigationOptions: {
+                    headerTransparent: true,
+                    headerTintColor: '#FFF',
+                    headerLeftContainerStyle: {
+                      marginLeft: 20,
+                    },
+                  },
+                }
+              ),
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: 'Agendar',
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255,255,255,0.6)"
+                  />
+                ),
+              },
+            },
             Profile,
           },
           {
