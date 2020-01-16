c1d8aa74f5016d97d829e057131a5900f2babcb9
Wenderson <wps713@gmail.com>
2020-01-16 13:30:08 -0300

    Add select date time page content

--- src/pages/NewAppointment/SelectDateTime/index.js
+++ src/pages/NewAppointment/SelectDateTime/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import { View } from 'react-native';
-
-// import { Container } from './styles';
+import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Background from '~/components/Background';
+import DateTimeInput from '~/components/DateTimeInput';
+import { Container } from './styles';
 
 export default function SelectDateTime() {
-  return <View />;
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <Background>
+      <Container>
+        <DateTimeInput date={date} onChange={setDate} />
+      </Container>
+    </Background>
+  );
 }
+
+SelectDateTime.navigationOptions = ({ navigation }) => ({
+  title: 'Selecione o horário',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#FFF" />
+    </TouchableOpacity>
+  ),
+});
