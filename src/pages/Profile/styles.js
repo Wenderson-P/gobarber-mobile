89959aa669842bbbbe43550489edb75398276f40
Wenderson <wps713@gmail.com>
2020-01-16 11:30:16 -0300

    Add Logout Button Style

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -34,3 +34,8 @@ export const FormInput = styled(Input)`
 export const SubmitButton = styled(Button)`
   margin-bottom: 5px;
 `;
+
+export const LogoutButton = styled(Button)`
+  margin-top: 5px;
+  background-color: #f64c75;
+`;
