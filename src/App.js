da8210e40faaeaa6a3bd9aa37a7ff95b2491b052
Wenderson <wps713@gmail.com>
2020-01-21 11:00:46 -0300

    Setting up navigation in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import createRouter from './routes';
+import NavigationServices from './services/navigation';
 
 export default function App() {
   const signed = useSelector(state => state.auth.signed);
   const Routes = createRouter(signed);
 
-  return <Routes />;
+  function registerService(ref) {
+    NavigationServices.setTopLevelNavigator(ref);
+  }
+  return <Routes ref={registerService} />;
 }
