15a5a147c4b00ca2cf1c2c70f4607af439375421
Wenderson <wps713@gmail.com>
2020-01-21 09:09:06 -0300

    Setting up One signal in app

--- src/index.js
+++ src/index.js
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StatusBar } from 'react-native';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import './config/reacototronConfig';
 import CodePush from 'react-native-code-push';
-
+import OneSignal from 'react-native-onesignal';
 import { store, persistor } from './store';
 
 import App from '~/App';
 
-function Index() {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-        <App />
-      </PersistGate>
-    </Provider>
-  );
+class Index extends Component {
+  constructor(props) {
+    super(props);
+    OneSignal.init('e5e1629f-b034-4d7b-a1ef-3c56de50d3e3');
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillMount() {
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  onReceived = data => {
+
+  }
+
+  onOpened = (notification) => {
+
+  }
+
+  onIds = (id) => {
+
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+          <App />
+        </PersistGate>
+      </Provider>
+    );
+  }
 }
 
 export default CodePush({
